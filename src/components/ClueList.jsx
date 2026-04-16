e451e3dfbf586133290644c01127fbc913993235
Anthony <anthony.matheusfrz@gmail.com>
2026-04-16 15:14:53 -0300

    penalizalçao por erro

--- src/components/ClueList.jsx
+++ src/components/ClueList.jsx
@@ -1,13 +1,27 @@
-export default function ClueList({ clues }) {
+export default function ClueList({ clues, maxVisible = 3, onRevealMore }) {
+  const visibleClues = clues.slice(0, maxVisible);
+  const hasMore = clues.length > maxVisible;
+
   return (
     <div className="panel">
       <div className="ptitle">▶ PISTAS DO CASO</div>
-      {clues.map((clue, i) => (
+
+      {visibleClues.map((clue, i) => (
         <div key={i} className="clue">
           <span className="clue-n">#{i + 1}</span>
           <span>{clue}</span>
         </div>
       ))}
+
+      {hasMore && (
+        <button
+          className="btn2 a"
+          onClick={onRevealMore}
+          style={{ marginTop: ".5rem", width: "100%" }}
+        >
+          🔍 VER MAIS PISTAS (- pontos)
+        </button>
+      )}
     </div>
   );
 }
